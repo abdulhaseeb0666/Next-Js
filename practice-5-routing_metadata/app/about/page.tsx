98153d77fdecdf6b9e6f54eac49f4f5cab567172
Abdul Haseeb <abdulhaseebah7886786@gmail.com>
2026-02-23 06:17:50 +0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   practice-5-routing_metadata/.gitignore
    	new file:   practice-5-routing_metadata/README.md
    	new file:   practice-5-routing_metadata/app/about/page.tsx
    	new file:   practice-5-routing_metadata/app/favicon.ico
    	new file:   practice-5-routing_metadata/app/global.d.ts
    	new file:   practice-5-routing_metadata/app/globals.css
    	new file:   practice-5-routing_metadata/app/layout.tsx
    	new file:   practice-5-routing_metadata/app/page.tsx
    	new file:   practice-5-routing_metadata/app/products/[productid]/page.tsx
    	new file:   practice-5-routing_metadata/app/products/page.tsx
    	new file:   practice-5-routing_metadata/next.config.ts
    	new file:   practice-5-routing_metadata/package-lock.json
    	new file:   practice-5-routing_metadata/package.json
    	new file:   practice-5-routing_metadata/postcss.config.mjs
    	new file:   practice-5-routing_metadata/public/file.svg
    	new file:   practice-5-routing_metadata/public/globe.svg
    	new file:   practice-5-routing_metadata/public/next.svg
    	new file:   practice-5-routing_metadata/public/vercel.svg
    	new file:   practice-5-routing_metadata/public/window.svg
    	new file:   practice-5-routing_metadata/tsconfig.json

---
+++ practice-5-routing_metadata/app/about/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: "About Page",
+}
+
+const page = () => {
+  return (
+    <div>
+        <h1 className="absolute text-9xl w-fit top-[50%] left-[50%] font-bold translate-x-[-50%] translate-y-[-50%]">About</h1>
+    </div>
+  )
+}
+
+export default page
