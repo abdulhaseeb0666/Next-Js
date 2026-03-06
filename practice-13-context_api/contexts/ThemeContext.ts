f44eb6012fc722e4ac4ba1a2ec7149ecb0eb3b5f
Abdul Haseeb <abdulhaseebah7886786@gmail.com>
2026-03-06 11:39:36 +0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   practice-13-context_api/.gitignore
    	new file:   practice-13-context_api/README.md
    	new file:   practice-13-context_api/app/components/ThemeButton.tsx
    	new file:   practice-13-context_api/app/favicon.ico
    	new file:   practice-13-context_api/app/global.d.ts
    	new file:   practice-13-context_api/app/globals.css
    	new file:   practice-13-context_api/app/layout.tsx
    	new file:   practice-13-context_api/app/page.tsx
    	new file:   practice-13-context_api/contexts/ThemeContext.ts
    	new file:   practice-13-context_api/contexts/ThemeProvider.tsx
    	new file:   practice-13-context_api/next.config.ts
    	new file:   practice-13-context_api/package-lock.json
    	new file:   practice-13-context_api/package.json
    	new file:   practice-13-context_api/postcss.config.mjs
    	new file:   practice-13-context_api/public/file.svg
    	new file:   practice-13-context_api/public/globe.svg
    	new file:   practice-13-context_api/public/next.svg
    	new file:   practice-13-context_api/public/vercel.svg
    	new file:   practice-13-context_api/public/window.svg
    	new file:   practice-13-context_api/tsconfig.json

---
+++ practice-13-context_api/contexts/ThemeContext.ts
@@ -0,0 +1,12 @@
+"use client";
+
+import { createContext } from "react";
+
+type ThemeContsextType = {
+    isDark: boolean,
+    toggleTheme: () => void
+}
+
+const ThemeContextAPI = createContext<ThemeContsextType | null>(null);  
+
+export default ThemeContextAPI;
